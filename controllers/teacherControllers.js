2eab3b625d02362d7c2994e5184be1152d0d6931
HammadiOuassim <ouassimhammadi@gmail.com>
2023-05-21 00:12:58 +0100

    post note error, techer not find

--- controllers/teacherControllers.js
+++ controllers/teacherControllers.js
@@ -6,14 +6,24 @@ const teacher = require('../models/teacherModele');
 const postNoteToStudent = async (req, res) => {
   try {
     const { studentId , note } = req.body;
-    
-    const teacherId = teacher.teacher_id;
+    const teacherId = parseInt(req.params.id);
+    //const teacherId = teacher.teacher_id;
 
     //res.json({message:'teacher id',teacherId})
 
 
-    //const subject = req.teacher.matiere;
+    //const subject = teacher.matiere;
 
+    const Fteacher = await teacher.findOne({ teacher_id: teacherId });
+    if (!Fteacher) {
+      return res.status(404).json({ error: 'Teacher not found' });
+    }
+
+    const matiere = Fteacher.matiere;
+
+
+
+    //
    
     const student = await students.findOne({ student_id: studentId });
 
@@ -21,10 +31,10 @@ const postNoteToStudent = async (req, res) => {
       return res.status(404).json({ error: 'Student not found' });
     }
 
-    student.notes.push({ teacher: teacherId, note });
+    student.notes.push({ teacher: teacherId, matiere,note });
     await student.save();
 
-    res.json({ success: true, message: 'Note posted successfully' });
+    res.json({ success: true, message: 'Note posted successfully' , matiere:matiere});
   } catch (error) {
     console.error('Error posting note:', error);
     res.status(500).json({ error: 'Server error' });
--- routes/teacherRouter.js
+++ routes/teacherRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const {postNoteToStudent,putNotes,getReclamation}=require('../controllers/teacherControllers')
 
-router.post("/students",postNoteToStudent)
+router.post("/:id/students",postNoteToStudent)
 //router.put("/students",putNotes)
 router.get("/reclamation/:id",getReclamation)
 module.exports=router;
